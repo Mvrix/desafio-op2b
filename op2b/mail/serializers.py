c25318b3d0cacd9e34af7ac66536125bab8f118a
Mario Elias <mario.junior@senior.com>
2025-08-18 00:52:25 -0300

    add first step

--- op2b/mail/serializers.py
+++ op2b/mail/serializers.py
@@ -5,3 +5,5 @@ from rest_framework import serializers
 
 class MailTextSerializer(serializers.Serializer):
     texto = serializers.CharField(max_length=1000, required=True)
+    destinatario = serializers.EmailField(required=True)
+    titulo = serializers.CharField(max_length=50, required=False, allow_blank=True)
--- op2b/mail/views.py
+++ op2b/mail/views.py
@@ -9,11 +9,32 @@ class MailFormatter(APIView):
     serializer = MailTextSerializer
 
     def post(self, request):
-        serializers.is_valid(raise_exception=True)
-        texto = serializers.validated_data['texto']
+        serializer = self.serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         
-        print("rexec", texto)
+        palavras = serializer.validated_data['texto'].replace('\n', ' ').split()
+        linhas = []
+        linha_atual = ""
+        result_text = ""
+        formated_text = ""
+        
+        for palavra in palavras:
+            if len(linha_atual) + len(palavra) + 1 <= 40:
+                formated_text += palavra + " "
+                if linha_atual:
+                    linha_atual += " " + palavra
+                else:
+                    linha_atual = palavra
+            else:
+                linhas.append(linha_atual)
+                linha_atual = palavra
 
-        resultado = {"texto_formatado": texto}
+        result_text = "\n".join(linhas)
+
+        resultado = {
+            "link para abrir caixa de e-mail": f"mailto:{serializer.validated_data['destinatario']}?subject={serializer.validated_data['titulo']}%20Formatado&body={result_text}",
+            "texto formatado": result_text,
+            "texto justificado": formated_text
+            }
 
         return Response(resultado, status=status.HTTP_200_OK)
